13351b6c075c44c48646cd216527433eddeb9b10
David Mielnik <davidmielnik@gmail.com>
2019-09-18 23:58:29 +0000

    Added mail delivery after succesful save in contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,10 @@ class ContactsController < ApplicationController
       @contact = Contact.new(contact_params)
       if @contact.save
          flash[:success] = "Mensaje enviado. Muchas gracias."
+         name = params[:contact][:name]
+         email = params[:contact][:email]
+         body = params[:contact][:comments]
+         ContactMailer.contact_email(name, email, body).deliver
          redirect_to new_contact_path
       else
          flash[:danger] = "Ocurrió un error: " + @contact.errors.full_messages.join(", ")
