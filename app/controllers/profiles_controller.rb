18c0cceda4388b509513000e263334ac42ac6cfe
David Mielnik <davidmielnik@gmail.com>
2019-09-24 23:14:36 +0000

    Added profile create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,24 @@
 class ProfilesController < ApplicationController
     def new
-        # Render blank profile form
+        # GET -> Render blank profile form
         @profile = Profile.new
     end
+    
+    def create
+        # POST -> Creates or updates profile to /users/:user_id/profile
+        # Ensure the user is building the profile
+        @user = User.find(params[:user_id])
+        @profile = @user.build_profile( profile_params )
+        if @profile.save
+            flash[:success] = "Perfil actualizado"
+            redirect_to root_path
+        else
+            render action: :new
+        end
+    end
+    
+    private
+        def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+        end
 end
