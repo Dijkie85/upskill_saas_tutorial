bab096568069009780c9bfd98b2551ffee98900a
David Mielnik <davidmielnik@gmail.com>
2019-09-24 01:28:30 +0000

    Added code comments for Stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,13 @@ class User < ApplicationRecord
          
   belongs_to :plan
   
+  # Extend Devise user registrations so that pro users save with special stripe
+  # Otherwise, Devise saves as usual
   attr_accessor :stripe_card_token
+  
   def save_with_subscription
+  # If pro users pass validations, call Stripe and have it set a monthly subscription 
+  # Stripe responds with customer data and we store in the DB
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
       self.stripe_customer_token = customer.id
