4b47a8a556979f7c607ea384a1280899130621cb
David Mielnik <davidmielnik@gmail.com>
2019-09-25 02:25:57 +0000

    Added file field for image upload

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,5 +4,5 @@ class Profile < ActiveRecord::Base
     has_attached_file :avatar, 
                       :styles => { :medium => "300x00>", :thumb => "100x100>" },
                       :default_url => "images/:style/missing.png"
-    validates_attachment_content_style :avatar, :content_type => /\Aimage\/.*\Z/
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
